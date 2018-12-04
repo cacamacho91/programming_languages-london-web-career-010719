0b485fca73042065cf2f25eb86a14e64238b7c09
dashing-frame-3501 <cacamacho91@gmail.com>
2018-12-04 15:24:25 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  reformat_languages = {}
+
+  languages.each do |style, languages|
+    binding.pry
+    languages.each do |language, language_data|
+      binding.pry
+      language_data.each do |language_key, language_value|
+        binding.pry
+      end
+    end
+  end
 end
